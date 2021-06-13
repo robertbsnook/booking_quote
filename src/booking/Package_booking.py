74a886aca62ea8a11811ecc4c6f9ab4ed2768755
robertbsnook <snookr@gmail.com>
2021-06-12 20:43:16 -0700

    commit package_booking -m "Can finally display shipping options!"

--- src/booking/Package_booking.py
+++ src/booking/Package_booking.py
@@ -11,47 +11,36 @@ class TravelRoute:
         self.destination = destination
         self.dangerous = dangerous
         self.urgency = urgency
-        self.length = length
-        self.width = width
-        self.height = height
-        self.weight = weight
+        self.length = float(length)
+        self.width = float(width)
+        self.height = float(height)
+        self.weight = float(weight)
 
-    def __str__(self,):
-        return self.
+    def __str__(self):
+        return str(self.charge)
 
 
 class Air(TravelRoute):
     def __init__(self,destination,dangerous,urgency,length,width,height,weight):
         super().__init__(destination,dangerous,urgency,length,width,height,weight)
 
-    def travel_check(self,):
-        if self.dangerous == "unsafe":
-            return False
-        else:
-            return True
-
-
     def charge(self):
-        if self.weight*10 > self.length*self.width*self.height*20:
+        if self.dangerous == "unsafe":
+            return 0
+        elif self.weight*10 > self.length*self.width*self.height*20:
             return float(self.weight*10)
         else:
-            return float(self.length*self.width*self.height*20)
+            return float(self.length * self.width * self.height * 20)
 
 
 class Truck(TravelRoute):
     def __init__(self,destination,dangerous,urgency,length,width,height,weight):
         super().__init__(destination,dangerous,urgency,length,width,height,weight)
 
-
-    def travel_check(self):
-        if self.destination == 'overseas':
-            return False
-        else:
-            return True
-
-
     def charge(self):
-        if self.urgency == 'urgent':
+        if self.destination == 'overseas':
+            return 0
+        elif self.urgency == 'urgent':
             return 45
         else:
             return 25
@@ -61,16 +50,13 @@ class Boat(TravelRoute):
     def __init__(self,destination,dangerous,urgency,length,width,height,weight):
         super().__init__(destination,dangerous,urgency,length,width,height,weight)
 
-    def travel_check(self):
-        if self.destination == 'c':
-            return False
-        elif self.urgency == 'urgent':
-            return False
-        else:
-            return True
-
     def charge(self):
-        return 30
+        if self.destination == 'c':
+            return 0
+        elif self.urgency == 'urgent':
+            return 0
+        else:
+            return 30
 
 
 def weight_check(weight):
@@ -129,6 +115,22 @@ def next_customer():
     else:
         return False
 
+def delivery_options(destination, dangerous, urgency, length, width, height, weight):
+    options = {}
+    air_option = Air(destination, dangerous, urgency, length, width, height, weight)
+    truck_option = Truck(destination, dangerous, urgency, length, width, height, weight)
+    boat_option = Boat(destination, dangerous, urgency, length, width, height, weight)
+    if air_option.charge() > 0.0:
+        options['Air'] = air_option.charge()
+    if truck_option.charge() > 0.0:
+        options['Truck'] = truck_option.charge()
+    if boat_option.charge() > 0.0:
+        options['Boat'] = boat_option.charge()
+    df2 = pd.DataFrame(list(options.items()), columns=['Option', 'Cost'])
+    print(tabulate(df2, tablefmt='psql'))
+
+
+
 
 def main():
     customer = True
@@ -160,30 +162,12 @@ def main():
         df.to_csv('booking_quotes.csv', index=True)
         row = df.tail(1).transpose()
         print(tabulate(row,tablefmt='psql'))
-
-        options = {}
-        air_option = Air(destination,dangerous,urgency,length,width,height,weight)
-
-        air_option = air_option.travel_check
-        print(air_option)
-        print(str(air_option))
-        print(Air.charge)
-        if Air.travel_check is True:
-            options.update({'Option': 'A','Flight cost': Air.charge})
-        if Truck.travel_check is True:
-            options.update({'Option': 'B','Truck cost': Truck.charge})
-        if Boat.travel_check is True:
-            options.update({'Option': 'C','Boat cost': Boat.charge})
-
-        print(options)
+        delivery_options(destination, dangerous, urgency, length, width, height, weight)
 
 
+# select option and have it append to the customer line
 
-                                                # show different shipping options as a dictionary
-                                                # select option and have it append to the customer line
-
-
-        print(df.last_valid_index())
+        print("Order ID:",df.last_valid_index())
         customer = next_customer()
 
 
