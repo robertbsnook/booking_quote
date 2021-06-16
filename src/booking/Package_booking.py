7d5cc46425acdad476f925b4d5103099665d6642
robertbsnook <snookr@gmail.com>
2021-06-15 22:37:24 -0700

    commit package_booking -m "Added in exception handling"

--- src/booking/Package_booking.py
+++ src/booking/Package_booking.py
@@ -6,13 +6,11 @@ from tabulate import tabulate
 
 
 class TravelRoute:
-    def __init__(self,destination,dangerous,urgency,length,width,height,weight):
+    def __init__(self, destination, dangerous, urgency, dimension, weight):
         self.destination = destination
         self.dangerous = dangerous
         self.urgency = urgency
-        self.length = float(length)
-        self.width = float(width)
-        self.height = float(height)
+        self.dimension = float(dimension)
         self.weight = float(weight)
 
     def __str__(self):
@@ -20,21 +18,21 @@ class TravelRoute:
 
 
 class Air(TravelRoute):
-    def __init__(self,destination,dangerous,urgency,length,width,height,weight):
-        super().__init__(destination,dangerous,urgency,length,width,height,weight)
+    def __init__(self, destination, dangerous, urgency, dimension, weight):
+        super().__init__(destination, dangerous, urgency, dimension, weight)
 
     def charge(self):
         if self.dangerous == "unsafe":
             return 0
-        elif self.weight*10 > self.length*self.width*self.height*20:
+        elif self.weight*10 > self.dimension*20:
             return float(self.weight*10)
         else:
-            return float(self.length * self.width * self.height * 20)
+            return float(self.dimension * 20)
 
 
 class Truck(TravelRoute):
-    def __init__(self,destination,dangerous,urgency,length,width,height,weight):
-        super().__init__(destination,dangerous,urgency,length,width,height,weight)
+    def __init__(self, destination, dangerous, urgency, dimension, weight):
+        super().__init__(destination, dangerous, urgency, dimension, weight)
 
     def charge(self):
         if self.destination == 'overseas':
@@ -46,8 +44,8 @@ class Truck(TravelRoute):
 
 
 class Boat(TravelRoute):
-    def __init__(self,destination,dangerous,urgency,length,width,height,weight):
-        super().__init__(destination,dangerous,urgency,length,width,height,weight)
+    def __init__(self, destination, dangerous, urgency, dimension, weight):
+        super().__init__(destination, dangerous, urgency, dimension, weight)
 
     def charge(self):
         if self.destination == 'in-country':
@@ -58,28 +56,11 @@ class Boat(TravelRoute):
             return 30
 
 
-def weight_check(weight):
-    if weight > 9.999:
-        print("Sorry, but this package weighs too much.  Please reduce the weight to under 10kg")
-        return False
-    else:
-        return True
-
-
-def size_check(length,width,height):
-    if length * width * height > 124.999:
-        print("Sorry, but this package is too large to be shipped by our methods.  Please reduce the size to less "
-              "than 5x5x5")
-        return False
-    else:
-        return True
 
 
 def urgent_check(delivery_date):
-    year, month, day = map(int, delivery_date.datetime.split('/'))
-    date = datetime.date(year, month, day)
-    future = datetime.date.today() + datetime.timedelta(days=3)
-    if date > future:
+    future = datetime.datetime.today() + datetime.timedelta(days=3)
+    if delivery_date > future:
         return "not urgent"
     else:
         return "urgent"
@@ -99,7 +80,6 @@ def destination_check():
 def danger_check():
     while True:
         danger = input("Does the package contain anything dangerous (y/n): ").lower()
-        # use Try and except to handle wrong letter input.
         if danger == 'n':
             return 'Safe'
         elif danger == 'y':
@@ -115,11 +95,12 @@ def next_customer():
     else:
         return False
 
-def delivery_options(destination, dangerous, urgency, length, width, height, weight):
+
+def delivery_options(destination, dangerous, urgency, dimension, weight):
     options = {}
-    air_option = Air(destination, dangerous, urgency, length, width, height, weight)
-    truck_option = Truck(destination, dangerous, urgency, length, width, height, weight)
-    boat_option = Boat(destination, dangerous, urgency, length, width, height, weight)
+    air_option = Air(destination, dangerous, urgency, dimension, weight)
+    truck_option = Truck(destination, dangerous, urgency, dimension, weight)
+    boat_option = Boat(destination, dangerous, urgency, dimension, weight)
     if air_option.charge() > 0.0:
         options['Air'] = air_option.charge()
     if truck_option.charge() > 0.0:
@@ -128,16 +109,27 @@ def delivery_options(destination, dangerous, urgency, length, width, height, wei
         options['Boat'] = boat_option.charge()
     df2 = pd.DataFrame(list(options.items()), columns=['Option', 'Cost'])
     print(tabulate(df2, tablefmt='psql'))
-    delivery_choice = int(input("Choose the delivery method:"))
-    df2_option = df2.at[delivery_choice,'Option']
-    df2_cost = df2.at[delivery_choice,'Cost']
-    return df2_option,df2_cost
+    selection = 0
+    while selection == 0:
+        try:
+            delivery_choice = input("Choose the delivery method:")
+            delivery_choice = int(delivery_choice)
+
+            if delivery_choice < 0 or delivery_choice > df2.last_valid_index():
+                print("Please select a valid method of transport")
+            else:
+                selection = 1
+        except ValueError:
+            print('Please enter a valid shipping option')
+    df2_option = df2.at[delivery_choice, 'Option']
+    df2_cost = df2.at[delivery_choice, 'Cost']
+    return df2_option, df2_cost
 
 
 def print_customer(df):
     row = df.tail(1).transpose()
-    print("Order ID:",df.last_valid_index())
-    print(tabulate(row,tablefmt='psql'))
+    print("Order ID:", df.last_valid_index())
+    print(tabulate(row, tablefmt='psql'))
 
 
 def get_name():
@@ -152,6 +144,7 @@ def get_name():
             print(e)
     return name
 
+
 def get_description():
     while True:
         try:
@@ -164,6 +157,7 @@ def get_description():
             print(e)
     return description
 
+
 def get_delivery_date():
     day = 0
     while day == 0:
@@ -175,8 +169,84 @@ def get_delivery_date():
             else:
                 day = 1
         except ValueError:
-            raise ValueError("Incorrect date format, should be YYY/MM/DD.")
+            print("Incorrect date format, should be YYYY/MM/DD.")
     return d_date
+
+
+@pysnooper.snoop()
+def get_dimensions():
+    print("Minimum dimension size is 0.1 meter.  Anything smaller should be rounded up to 0.1.\n"
+          "Minimum overall size is 0.5m")
+    while True:
+        try:
+            length = float(input("L: "))
+            if not length:
+                raise ValueError("Please enter a length.")
+            elif length <0.1:
+                print("Please enter a dimension greater than 0.0999.")
+            else:
+                break
+        except ValueError as e:
+            print(e)
+    while True:
+        try:
+            width = float(input("W: "))
+            if not width:
+                raise ValueError("Please enter a width.")
+            elif width <0.1:
+                print("Please enter a dimension greater than 0.0999.")
+            else:
+                break
+        except ValueError as e:
+            print(e)
+    while True:
+        try:
+            height = float(input("H: "))
+            if not height:
+                raise ValueError("Please enter a height.")
+            elif height <0.1:
+                print("Please enter a dimension greater than 0.0999.")
+            else:
+                break
+        except ValueError as e:
+            print(e)
+    if length*width*height < 0.5:
+        dimension = 0.5
+    else:
+        dimension = length*width*height
+    return dimension
+
+
+def size_check(dimension):
+    if dimension > 124.999:
+        print("Sorry, but this package is too large to be shipped by our methods.  Please reduce the size to less "
+              "than 5x5x5")
+        return False
+    else:
+        return True
+
+
+def get_weight():
+    while True:
+        try:
+            weight = float(input("How many kilograms does it weigh: "))
+            if not weight:
+                raise ValueError("Please enter a weight.  Cannot be blank")
+            elif weight <=0:
+                print("Please enter a positive weight.")
+            else:
+                break
+        except ValueError as e:
+            print(e)
+    return weight
+
+
+def weight_check(weight):
+    if weight > 9.999:
+        print("Sorry, but this package weighs too much.  Please reduce the weight to under 10kg")
+        return False
+    else:
+        return True
 
 
 def main():
@@ -188,46 +258,31 @@ def main():
         dangerous = danger_check()
         delivery_date = get_delivery_date()
         urgency = urgent_check(delivery_date)
-        try:
-            weight = input("Weight in kilograms: ")
-        except ValueError:
-            print("Please input a proper weight. ")
-        try:
-            length = input("L: ")
-        except ValueError:
-            print("Please input a proper length. ")
-        try:
-            width = input("W: ")
-        except ValueError:
-            print("Please input a proper width. ")
-        try:
-            height = input("H: ")
-        except ValueError:
-            print("Please input a proper height. ")
-        df = pd.read_csv('booking_quotes.csv', index_col= 0)
+        weight = get_weight()
+        weight_check(weight)
+        dimension = get_dimensions()
+        df = pd.read_csv('booking_quotes.csv', index_col=0)
         df.index.name = 'ID'
         df = df.reset_index(drop=True)
         new_row = {'Customer_Name': customer_name.title(),
-               'Destination': destination,
-               'Package_desc': package_desc,
-               'Dangerous': dangerous,
-               'Delivery_date': delivery_date,
-               'Urgency': urgency,
-               'Weight': weight,
-               'Length': length,
-               'Width': width,
-               'Height': height}
-        df = df.append(new_row,ignore_index=True)
+                'Destination': destination,
+                'Package_desc': package_desc,
+                'Dangerous': dangerous,
+                'Delivery_date': delivery_date,
+                'Urgency': urgency,
+                'Weight': weight,
+                'Size': round(dimension,2),
+                'Shipping_option': '',
+                'Cost': ''}
+        df = df.append(new_row, ignore_index=True)
         print_customer(df)
 
-        d_option,d_cost = delivery_options(destination, dangerous, urgency, length, width, height, weight)
+        d_option, d_cost = delivery_options(destination, dangerous, urgency, dimension, weight)
         df.at[df.last_valid_index(), 'Shipping_option'] = d_option
         df.at[df.last_valid_index(), 'Cost'] = d_cost
         df.to_csv('booking_quotes.csv', index=True)
         print_customer(df)
         customer = next_customer()
-
-
 
 
 if __name__ == "__main__":
